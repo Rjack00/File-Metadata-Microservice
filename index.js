cb503f2922d695f8a005b0af58738330e1a18f90
Rjack00 <Rjackson77@comcast.net>
2025-12-16 18:35:31 -0700

    index.js updated
    added post with multer

--- index.js
+++ index.js
@@ -8,11 +8,24 @@ const app = express();
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+const upload = multer({dest: 'uploads/'});
+
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
 
+  if(!req.file) {
+    return res.json({error: 'No file uploaded'});
+  }
+
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+})
 
 
 const port = process.env.PORT || 3000;
